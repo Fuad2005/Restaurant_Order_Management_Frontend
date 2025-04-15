1faa8215fe4c37a02e20e9ef17b02d4f08835979
Fuad2005 <fuadqasimov000@gmail.com>
2025-04-15 17:17:12 +0400

    inital config done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router';
+
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
